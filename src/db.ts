3ea9a3d22396cafa872cf23ef30e4fcda3eaddf9
jquinzell <>
2023-10-25 15:07:14 -0500

    types

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,17 @@
-import {Prisma } from '@prisma/client';
-import { PrismaClient } from '@prisma/client'
-export const db = new PrismaClient()
+import { Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+export const db = new PrismaClient();
+
+const board = Prisma.validator<Prisma.BoardDefaultArgs>()({});
+
+export type Board = Prisma.BoardGetPayload<typeof board>;
 
 const listWithCards = Prisma.validator<Prisma.ListDefaultArgs>()({
   include: { cards: true },
-})
+});
 
-export type List = Prisma.ListGetPayload<typeof listWithCards>
+export type List = Prisma.ListGetPayload<typeof listWithCards>;
 
-const card = Prisma.validator<Prisma.CardDefaultArgs>()({})
+const card = Prisma.validator<Prisma.CardDefaultArgs>()({});
 
-export type Card = Prisma.CardGetPayload<typeof card>
+export type Card = Prisma.CardGetPayload<typeof card>;
