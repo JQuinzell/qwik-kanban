ebac6e3b48407ac6124465c0859ef307063aa841
jquinzell <>
2023-10-25 15:05:49 -0500

    create board

---
+++ src/components/CreateBoard.tsx
@@ -0,0 +1,54 @@
+import type { PropFunction } from "@builder.io/qwik";
+import { $, component$, useSignal } from "@builder.io/qwik";
+
+interface Props {
+  onCreate$: PropFunction<(name: string) => void>;
+}
+export const CreateBoard = component$((props: Props) => {
+  const creating = useSignal(false);
+  const name = useSignal("");
+
+  const clear = $(() => {
+    creating.value = false;
+    name.value = "";
+  });
+
+  return (
+    <>
+      <div
+        onClick$={() => (creating.value = true)}
+        class="flex h-24 justify-center rounded-sm bg-gray-300 "
+      >
+        <div class="my-auto text-center text-sm text-black">
+          Create New Board
+        </div>
+      </div>
+      {creating.value && (
+        <div>
+          <div class="flex">
+            <p>Create a board</p>
+            <button class="ml-auto" onClick$={clear}>
+              X
+            </button>
+          </div>
+          <hr />
+          <input
+            type="text"
+            placeholder="Name"
+            value={name.value}
+            onChange$={(ev, el) => (name.value = el.value)}
+          />
+          <button
+            class="btn"
+            onClick$={() => {
+              props.onCreate$(name.value);
+              clear();
+            }}
+          >
+            Create
+          </button>
+        </div>
+      )}
+    </>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,45 +1,53 @@
-import { component$ } from '@builder.io/qwik'
-import { routeLoader$, type DocumentHead } from '@builder.io/qwik-city'
-import { BoardCard } from '~/components/BoardCard'
-import { db } from '~/db'
+import { component$ } from "@builder.io/qwik";
+import {
+  routeLoader$,
+  type DocumentHead,
+  routeAction$,
+} from "@builder.io/qwik-city";
+import { BoardCard } from "~/components/BoardCard";
+import { CreateBoard } from "~/components/CreateBoard";
+import { db } from "~/db";
 
 export const useBoards = routeLoader$(async () => {
-  const boards = await db.board.findMany()
-  return boards
-})
+  const boards = await db.board.findMany();
+  return boards;
+});
+
+export const useCreateBoard = routeAction$(async (data) => {
+  await db.board.create({ data: { name: data.name as string } });
+});
 
 export default component$(() => {
-  const boardsSignal = useBoards()
-  console.log(boardsSignal.value)
-  // const boards = useRouteData<typeof routeData>()
-  // const [_, handleCreateBoard] = createRouteAction(async (name: string) => {
-  //   await client.createBoard.mutate(name)
-  // })
+  const boardsSignal = useBoards();
+  const createBoardAction = useCreateBoard();
 
   return (
-    <main class='grid grid-cols-[300px_1fr]'>
+    <main class="grid grid-cols-[300px_1fr]">
       <nav></nav>
       <div>
-        <h2 class='text-left text-xl font-bold mb-4'>Boards</h2>
-        <div class='grid-cols-4 grid gap-x-4 gap-y-8'>
+        <h2 class="mb-4 text-left text-xl font-bold">Boards</h2>
+        <div class="grid grid-cols-4 gap-x-4 gap-y-8">
           {boardsSignal.value.map((board) => (
             <BoardCard key={board.id} board={board} />
           ))}
 
-          {/* <For each={boards()}>{(board) => <BoardCard board={board} />}</For> */}
-          {/* <CreateBoard onCreate={handleCreateBoard} /> */}
+          <CreateBoard
+            onCreate$={(name) => {
+              createBoardAction.submit({ name });
+            }}
+          />
         </div>
       </div>
     </main>
-  )
-})
+  );
+});
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
-}
+};
